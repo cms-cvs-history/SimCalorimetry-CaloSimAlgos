6d23da08a8f323fb4980e8131de9b42c28d28e67
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2006-11-27 17:58:06 +0000

    optimization for sparse matrix handling (courtesy of V. Innocente) added check on the matrix being diagonal or not

--- test/CaloSimAlgosTest.cpp
+++ test/CaloSimAlgosTest.cpp
@@ -7,12 +7,21 @@ int main()
   CaloCorrelatedNoisifier noisifier(10);
  
   DetId detId;
-  CaloSamples samples(detId, 10);
 
   noisifier.setDiagonal(1);
   noisifier.setOffDiagonal(1, -0.2);
+  noisifier.setOffDiagonal(2, -0.1);
 
+  std::cout <<  noisifier.matrix() << std::endl;
+
+  CaloSamples samples(detId, 10);
   noisifier.noisify(samples);
   std::cout << samples << std::endl;
+
+  for (int i=0; i<400000; i++) {
+    noisifier.noisify(samples);
+  }
+  std::cout << samples << std::endl;
+
 }
 
