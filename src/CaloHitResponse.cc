00347f226ab5ec862ab46ed4136dedde58572882
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2011-08-29 18:26:44 +0000

    Use CaloSamples +=

--- src/CaloHitResponse.cc
+++ src/CaloHitResponse.cc
@@ -102,16 +102,9 @@ void CaloHitResponse::add(const CaloSamples & signal)
   DetId id(signal.id());
   CaloSamples * oldSignal = findSignal(id);
   if (oldSignal == 0) {
-    theAnalogSignalMap[id] = signal;
+     theAnalogSignalMap[id] = signal;
   } else  {
-    // need a "+=" to CaloSamples
-    int sampleSize =  oldSignal->size();
-    assert(sampleSize == signal.size());
-    assert(signal.presamples() == oldSignal->presamples());
-
-    for(int i = 0; i < sampleSize; ++i) {
-      (*oldSignal)[i] += signal[i];
-    }
+    *oldSignal += signal;
   }
 }
 
